d0adf6bdd3d0f070c4399f939916a29559b1b9b0
Lera Zotova <zotovavaleria35@gmail.com>
2026-04-30 12:28:22 +0300

    Лаба 4(апдейт)

--- auto_main/admin.py
+++ auto_main/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from .models import Brand, Country, CarModel # Імпортуємо тільки те, що є в models.py
+from .models import Brand, Country, CarModel
 
 @admin.register(CarModel)
 class CarModelAdmin(admin.ModelAdmin):
-    # Виводимо назву моделі, марку та дати
-    list_display = ('name', 'brand', 'created_at', 'updated_at')
+    # Просто виводимо ціну як є, вона вже буде з ($) у заголовку
+    list_display = ('name', 'brand', 'price', 'created_at', 'updated_at')
 
 admin.site.register(Brand)
 admin.site.register(Country)
--- auto_main/models.py
+++ auto_main/models.py
@@ -1,31 +1,21 @@
 from django.db import models
 
-
-# 1. Таблиця Країна
 class Country(models.Model):
     name = models.CharField(max_length=50, verbose_name="Країна")
+    def __str__(self): return self.name
 
-    def __str__(self):
-        return self.name
-
-
-# 2. Таблиця Марка
 class Brand(models.Model):
     name = models.CharField(max_length=50, verbose_name="Марка")
-    country = models.ForeignKey(Country, on_delete=models.CASCADE, verbose_name="Країна")
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+    def __str__(self): return self.name
 
-    def __str__(self):
-        return self.name
-
-
-# 3. Таблиця Модель (Об'єднуємо тут)
 class CarModel(models.Model):
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE, verbose_name="Марка")
     name = models.CharField(max_length=50, verbose_name="Назва моделі")
-
-    # Поля для лаби
+    # Додали ($) в назву поля
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0, verbose_name="Ціна ($)")
+    description = models.TextField(default='', verbose_name="Опис")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Створено о")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="Оновлено о")
 
-    def __str__(self):
-        return self.name
+    def __str__(self): return self.name
